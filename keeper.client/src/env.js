932b6cab0ec644cda2fab32bd3f95a38fe678735
pkrueger <sendthistopatrick@gmail.com>
2022-11-04 11:19:01 -0600

    Set up auth 0 and sql db connections

--- keeper.client/src/env.js
+++ keeper.client/src/env.js
@@ -1,6 +1,6 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const dev = window.location.origin.includes("localhost");
+export const baseURL = dev ? "http://localhost:7045" : "";
+export const useSockets = false;
+export const domain = "pkrueger.us.auth0.com";
+export const clientId = "z1g4pgjFJmr5JGyzBJeqGtMwqaGl75RR";
+export const audience = "https://learning-stuff.com";
