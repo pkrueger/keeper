42d8358f000157ea3165e468ef5f5fa7505497f5
pkrueger <sendthistopatrick@gmail.com>
2022-11-04 11:39:33 -0600

    SQL setup

--- keeper/dbSetup.sql
+++ keeper/dbSetup.sql
@@ -1,8 +1,45 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8 COMMENT '';
+-- Active: 1666715598134@@SG-kiwi-myrtle-9554-6832-mysql-master.servers.mongodirector.com@3306@stuff
+
+CREATE TABLE
+    IF NOT EXISTS accounts(
+        id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        name varchar(255) COMMENT 'User Name',
+        email varchar(255) COMMENT 'User Email',
+        picture varchar(255) COMMENT 'User Picture',
+        coverImg VARCHAR(255) COMMENT 'User Cover Image'
+    ) default charset utf8mb4 COMMENT '';
+
+CREATE TABLE
+    IF NOT EXISTS vaults(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        creatorId VARCHAR(255) NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        description VARCHAR(255) NOT NULL,
+        coverImg VARCHAR(255) NOT NULL,
+        isPrivate BOOLEAN NOT NULL,
+        FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+    ) DEFAULT charset utf8mb4;
+
+CREATE TABLE
+    IF NOT EXISTS keeps(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        creatorId VARCHAR(255) NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        description VARCHAR(255) NOT NULL,
+        img VARCHAR(255) NOT NULL,
+        views INT DEFAULT 0,
+        FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+    ) DEFAULT charset utf8mb4;
+
+CREATE TABLE
+    IF NOT EXISTS vaultKeeps(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        creatorId VARCHAR(255) NOT NULL,
+        vaultId INT NOT NULL,
+        keepId INT NOT NULL,
+        FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE,
+        FOREIGN KEY (vaultId) REFERENCES vaults(id) ON DELETE CASCADE,
+        FOREIGN Key (keepId) REFERENCES keeps(id) ON DELETE CASCADE
+    ) DEFAULT charset utf8mb4;
